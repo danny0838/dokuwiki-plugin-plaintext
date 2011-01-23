30bb4853e0ec0485bcca84e6b899634a516cca84
Danny Lin <danny0838@pchome.com.tw>
2011-01-23 09:15:23 +0800

    First release

---
+++ syntax.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * <text> tag that embeds plain text with linebreaks
+ */
+
+// must be run within Dokuwiki
+if(!defined('DOKU_INC')) die();
+
+/**
+ * All DokuWiki plugins to extend the parser/rendering mechanism
+ * need to inherit from this class
+ */
+class syntax_plugin_plaintext extends DokuWiki_Syntax_Plugin {
+
+    function getType() { return 'protected';}
+    function getPType() { return 'normal';}
+    function getSort() { return 20; }
+
+    /**
+     * return some info
+     */
+    function getInfo(){
+        return array(
+            'author' => 'Danny Lin',
+            'email'  => 'danny0838[at]pchome[dot]com[dot]tw',
+            'date'   => '2011-01-01',
+            'name'   => 'Plain Text',
+            'desc'   => '<text> tag that embeds plain text with linebreaks.',
+            'url'    => 'http://www.dokuwiki.org/plugin:plaintext',
+        );
+    }
+
+    /**
+     * Connect pattern to lexer
+     */
+    function connectTo($mode) {
+        $this->Lexer->addSpecialPattern('<text>.*?</text>', $mode, 'plugin_plaintext');
+    }
+
+    /**
+     * Handle the match
+     */
+    function handle($match, $state, $pos, &$handler){
+        return substr($match,6,-7);
+    }
+
+    /**
+     * Create output
+     */
+    function render($format, &$renderer, $data) {
+        if($format == 'xhtml'){
+            $renderer->doc .= str_replace( "\n", "<br/>", trim($renderer->_xmlEntities($data),"\n") );
+            return true;
+        }
+        return false;
+    }
+}
